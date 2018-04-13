158404dd9e6deac6eb9bc01797893f71268e968c
Kristina Riemer <kristina.riemer@weecology.org>
2018-04-13 12:25:16 -0400

    Add route location map, start counting number species

--- scripts/cleaning_BBS.R
+++ scripts/cleaning_BBS.R
@@ -1,6 +1,9 @@
 library(rdataretriever)
+library(ggplot2)
+library(dplyr)
 
 #-------FUNCTIONS---------
+#TODO: doc string
 download_BBS = function(raw_file_path){
   if(!file.exists("raw_data/breed_bird_survey_counts.csv")){
     rdataretriever::install("breed-bird-survey", "csv", data_dir = "raw_data/")
@@ -10,9 +13,23 @@ download_BBS = function(raw_file_path){
 #-------RUN FUNCTIONS---------
 download_BBS("raw_data/breed_bird_survey_counts.csv")
 
-#one route = one site? 
-BBS_counts = read.csv("raw_data/breed_bird_survey_counts.csv")
+# Read in all BBS datasets
+BBS_counts = read.csv("raw_data/breed_bird_survey_counts.csv") #each row is one species per one route
 BBS_region_codes = read.csv("raw_data/breed_bird_survey_region_codes.csv") #region codes for each US state & CA province
 BBS_routes = read.csv("raw_data/breed_bird_survey_routes.csv") #location of each route, >5000 total
 BBS_species = read.csv("raw_data/breed_bird_survey_species.csv") #species codes & taxonomic info
 BBS_weather = read.csv("raw_data/breed_bird_survey_weather.csv")
+
+# Exploratory stats & figures
+ggplot(data = BBS_routes, aes(x = longitude, y = latitude)) +
+  borders("world") +
+  geom_point(size = 0.1, color = "red", alpha = 0.5)
+
+ggplot(data = BBS_routes, aes(x = longitude, y = latitude)) +
+  borders("usa") +
+  geom_point(size = 0.3, color = "red", alpha = 0.5)
+
+#one country, one state, one route, one year
+test_site = BBS_counts %>% 
+  filter(countrynum == 840, statenum == 2, route == 1, year == 1967) %>% #TODO: change to groupby
+  summarize(number_species = n())
